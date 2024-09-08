35ea7b0dd074f3c106c2d04e68dfe1b3d8cb2bc4
Marcos Bião <dev.marcosdly@gmail.com>
2024-09-08 20:11:02 -0300

    feat(style): color type annotations and utility classes

---
+++ src/types/style.d.ts
@@ -0,0 +1,12 @@
+export type ColorOption =
+  | 'red'
+  | 'orange'
+  | 'yellow'
+  | 'light-lavender'
+  | 'champagne'
+  | 'pastel-green'
+  | 'blue'
+  | 'ocean-green'
+  | 'green'
+  | 'white'
+  | 'black';
