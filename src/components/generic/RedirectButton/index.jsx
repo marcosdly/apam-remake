0cd895e55911dea1197fddb994e7977ca2d9a19e
Marcos Bião <dev.marcosdly@gmail.com>
2024-09-08 20:22:49 -0300

    feat(component): add RedirectButton

---
+++ src/components/generic/RedirectButton/index.jsx
@@ -0,0 +1,33 @@
+import { useRef } from 'preact/hooks';
+import './redirect-button.scss';
+
+/** @typedef {import('@/types/style.d.ts').ColorOption} ColorOption */
+/** @typedef {import('preact/compat')} React */
+
+/**
+ * @typedef RedirectButtonProps
+ * @property {string} href Link to redirect to.
+ * @property {string} text Button's text content.
+ * @property {ColorOption} color Background color's name.
+ * @property {'pill' | 'round-corners'} shape Button's shape.
+ */
+
+/** @param {RedirectButtonProps} props */
+export default function RedirectButton(props) {
+  const { href, text, shape, color } = props;
+
+  /** @type {React.RefObject<HTMLAnchorElement>} */
+  const link = useRef();
+
+  return (
+    <button
+      type="button"
+      className={`redirect-button font-sans ${shape} bg-${color}`}
+      onClick={() => link.current?.click?.()}
+    >
+      <a ref={link} className="link" href={href}>
+        {text}
+      </a>
+    </button>
+  );
+}
