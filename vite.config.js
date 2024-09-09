ac281e5ee3e693aa4806c371a54e76cfc5172718
Marcos Bião <dev.marcosdly@gmail.com>
2024-09-09 00:44:32 -0300

    feat(style): custom sass function to generate css filters

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 import { ViteMinifyPlugin } from 'vite-plugin-minify';
+import { hexToCSSFilter } from 'hex-to-css-filter';
+import { SassString } from 'sass';
 const { assign } = Object;
 
 /** @typedef {import('vite').UserConfig} UserConfig */
+
+const customSassFunctions = {
+  'hex-to-filter($color)': (color) => {
+    const converted = hexToCSSFilter(color.dartValue.toString(), {
+      acceptanceLossPercentage: 2,
+      maxChecks: 200,
+    });
+    return new SassString(converted.filter, { quotes: false });
+  },
+};
 
 export default defineConfig(({ mode }) => {
   /** @type {UserConfig} */
@@ -15,6 +27,13 @@ export default defineConfig(({ mode }) => {
       sourcemap: true,
       rollupOptions: {
         input: ['./index.html'],
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          functions: customSassFunctions,
+        },
       },
     },
   };
