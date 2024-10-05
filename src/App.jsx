d92781d51a1279547a9a431ceb486cf0e60243b7
Marcos Bião <dev.marcosdly@gmail.com>
2024-10-05 20:40:45 -0300

    feat: add dummy entrypoint

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Header from './components/generic/Header';
+
+import './style/normalize.scss';
+import './style/fonts.scss';
+import './style/color.scss';
+
+export function App() {
+  return <Header />;
+}
