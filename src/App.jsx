c530c12ae23b447d559aaeeb66e22ca1929bd853
Marcos Bião <dev.marcosdly@gmail.com>
2024-10-06 15:46:02 -0300

    chore: adapt entrypoint to preact router interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import Header from './components/generic/Header';
+import { LocationProvider, Route, Router } from 'preact-iso';
+import Home from './pages/Home';
 
 import './style/normalize.scss';
 import './style/fonts.scss';
 import './style/color.scss';
 
 export function App() {
-  return <Header />;
+  return (
+    <LocationProvider>
+      <Router>
+        <Route default component={Home} />
+        <Route path="/" component={Home} />
+      </Router>
+    </LocationProvider>
+  );
 }
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,5 @@
+import Header from 'components/generic/Header';
+
+export default function Home() {
+  return <Header />;
+}
