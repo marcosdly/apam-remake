d2ba029be785eae2124dbbd95bad552f460b6b84
Marcos Bião <dev.marcosdly@gmail.com>
2024-10-12 17:04:01 -0300

    chore(style): add page header back in

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
 import Intro from 'components/Home/Intro';
 import './home.scss';
+import Header from 'components/generic/Header';
 
 export default function Home() {
   return (
     <div className="page home">
+      <Header />
       <Intro />
     </div>
   );
